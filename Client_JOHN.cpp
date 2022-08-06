7bc4efd98a7aa00edd898adaaa0db1bb6c0c03b7
John Rock <88595192+koteoo@users.noreply.github.com>
2022-08-06 22:04:26 +0500

    Add files via upload

---
+++ Client_JOHN.cpp
@@ -0,0 +1,73 @@
+﻿// Client_JOHN.cpp UDP
+
+#include "pch.h"
+#include <iostream>
+#include <Winsock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib,"Ws2_32.lib")
+using namespace std;
+
+#define MESSAGE_BUFFER 4096
+#define PORT 7777
+
+char buffer[MESSAGE_BUFFER];
+char message[MESSAGE_BUFFER];
+
+SOCKET sockd;
+struct sockaddr_in server_address;
+WSAData wsadata;
+
+
+void clientRequest() {
+	//--------Checking of systems Windows ------
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+		cout << "Error WinSock version initializaion #";
+		cout << WSAGetLastError();
+		exit(1);
+	}
+	//------------------------------------------
+
+	//-------- The filling into a struct of the serveraddress ---------
+	
+	//server_address.sin_addr.s_addr = inet_addr ("127.0.0.1"); -- Так уже нельзя
+	inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr.s_addr);//Preferred option
+	server_address.sin_port = htons(PORT);
+	server_address.sin_family = AF_INET;
+	//-----------------------------------------------------------------
+
+
+	//------- Creating of the Socket ----------
+	sockd = socket(AF_INET,SOCK_DGRAM,0);
+	if (sockd == INVALID_SOCKET) {//  Socket's Checking   
+		cout << "Error initialization socket # " << WSAGetLastError() << endl;
+		closesocket(sockd);
+		WSACleanup();
+		exit(1);
+	}
+	else {
+		cout << "Client socket initialization is OK" << endl;
+	}
+	//------------------------------------------
+
+
+	//--- Recieving and sending of messages ------------
+	cout << "Enter your request to the server: ";
+	cin >> message;
+	sendto(sockd, message, MESSAGE_BUFFER, 0, (sockaddr*)&server_address, sizeof(server_address));
+	recvfrom(sockd, buffer, sizeof(buffer),0,0,0);
+	cout << "server's message: " << buffer << endl;
+	closesocket(sockd);
+	//--------------------------------------------------
+
+	closesocket(sockd);
+	WSACleanup();
+
+};
+
+
+
+int main()
+{
+	cout << "Client starting:" << endl;
+	clientRequest();
+}
---
+++ Server_JOHN.cpp
@@ -0,0 +1,91 @@
+﻿// Server_JOHN.cpp UDP
+#include "pch.h"
+#include <iostream>
+#include <Winsock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib,"Ws2_32.lib")
+using namespace std;
+
+#define MESSAGE_BUFFER 4096
+#define PORT 7777
+
+
+char buffer[MESSAGE_BUFFER];
+char message[MESSAGE_BUFFER];
+
+SOCKET sockd;
+struct sockaddr_in serveraddress, client;//Structs
+int errState;//State of ERR
+WSAData wsadata; // Структура WSADATA содержит сведения о реализации сокетов Windows.
+
+//Параметр MAKEWORD(2, 2) WSAStartup запрашивает версию 2.2 Winsock в системе
+//и задает переданную версию в качестве самой высокой версии Windows Sockets поддержки,
+//которую может использовать вызывающий объект.
+
+void processRequest() {
+	
+	//--------Checking of systems Windows ------
+	errState = WSAStartup(MAKEWORD(2, 2), &wsadata);// Функция WSAStartup вызывается для запуска использования WS2_32.dll.
+
+	if (errState != 0) {
+		cout << "Error WinSock version initializaion #";
+		cout << WSAGetLastError();
+		exit(1);
+	}
+	
+	//------------------------------------------
+
+	//------- Creating of the Socket ----------
+	sockd = socket(AF_INET,SOCK_DGRAM,0);
+			//or (sockd == -1)
+	if (sockd == INVALID_SOCKET) {//  Socket's Checking   
+		cout << "Error initialization socket # " << WSAGetLastError() << endl;
+		closesocket(sockd);
+		WSACleanup();
+		exit(1);
+	}
+	else {
+		cout << "Server socket initialization is OK" << endl;
+	}
+	//------------------------------------------
+	
+	//-------- The filling into a struct of the serveraddress ---------
+
+	serveraddress.sin_family = AF_INET;
+	serveraddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddress.sin_port = htons(PORT);
+
+	//-----------------------------------------------------------------
+
+
+	//-------------- Sock's binding -----------------------------------
+	//Для привязки созданного сокета к IP-адресу и номеру порта служит системный вызов bind().
+				//	|---Приведение типа sockaddr_in К sockaddr
+	bind(sockd, (sockaddr*)&serveraddress, sizeof(serveraddress));
+
+	//-----------------------------------------------------------------
+	int length = sizeof(client);// This var needed for client's length  
+	
+	
+	
+	//--- Recieving and sending of messages ------------
+	recvfrom(sockd,buffer,sizeof(buffer), 0, (struct sockaddr*)&client, &length);
+	
+	cout << "Client's message: "<< buffer << endl;
+	cout << "Enter message to client: ";
+	cin >> message;
+	sendto(sockd,message,sizeof(message),0, (sockaddr*)&client, sizeof(client));
+
+	//--------------------------------------------------
+	
+	closesocket(sockd);
+	WSACleanup();
+}
+
+int main() {
+
+	cout<< "Starting serer: " <<endl;
+	processRequest();
+
+	return 0;
+}
